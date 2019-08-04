fb67b4c9a40c27a30ed52b162898b9e34a74c125
Aman Jain <AmanJa@hcl.com>
2019-08-04 20:27:16 +0530

    footer changes

--- src/shared/footer/footer.js
+++ src/shared/footer/footer.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 
 import "./_footer.scss";
-import logoFooter from "../../assets/images/pepsico_Logo.png";
-
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer className="footer-row">
-                <img src={logoFooter} alt="" />
-            </footer>
-        );
-    }
+  render() {
+    return (
+      <footer className="footer-row">
+        <div className="row d-flex float-left justify-content-center footer-row-inner">
+          <div className="col-3 col-md-3 col-sm-3 col-xs-3">
+            <i className="fa fa-home footer-icons" aria-hidden="true" />
+          </div>
+
+          <div className="col-3 col-md-3 col-sm-3 col-xs-3">
+            <i className="fa fa-industry footer-icons" aria-hidden="true" />
+          </div>
+          <div className="col-3 col-md-3 col-sm-3 col-xs-3">
+            <i className="fa fa-industry footer-icons" aria-hidden="true" />
+          </div>
+          <div className="col-3 col-md-3 col-sm-3 col-xs-3">
+            <i className="fa fa-industry footer-icons" aria-hidden="true" />
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
