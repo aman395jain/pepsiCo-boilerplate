f063b15282a349f039906ccce5ecfbbe5a5a6c4a
Aman Jain <AmanJa@hcl.com>
2019-08-06 17:43:19 +0530

    footer changes

--- src/shared/footer/footer.js
+++ src/shared/footer/footer.js
@@ -4,32 +4,31 @@ import "./_footer.scss";
 
 class Footer extends Component {
   render() {
-    console.log("in the footer", this.props.isAuthorized)
-    return (
-      this.props.isAuthorized ?
-        (<footer className="footer-row">
-          <div className="row d-flex justify-content-center footer-row-inner">
-            <div className="col-3 col-md-3 col-sm-3 col-xs-3">
-              <i className="fa fa-home footer-icons" aria-hidden="true" >
-                <span className="footer-text">Home</span></i>
-            </div>
-
-            <div className="col-3 col-md-3 col-sm-3 col-xs-3">
-              <i className="fa fa-cubes footer-icons" aria-hidden="true" >
-                <span className="footer-text">Racks</span>
-              </i>
-            </div>
-            <div className="col-3 col-md-3 col-sm-3 col-xs-3">
-              <i className="fa fa-qrcode footer-icons" aria-hidden="true" ><span className="footer-text">QrScan</span>
-              </i>
-            </div>
-            <div className="col-3 col-md-3 col-sm-3 col-xs-3">
-              <i className="fa fa-shopping-cart footer-icons" aria-hidden="true" ><span className="footer-text">Cart</span>
-              </i>
-            </div>
+    console.log("in the footer", this.props.isAuthorized);
+    return this.props.isAuthorized ? (
+      <footer className="footer-row d-flex footer-row-inner justify-content-between">
+          <div className="icon-row">
+            <i className="fa fa-home footer-icons" aria-hidden="true" />
+            <span className="footer-text">Home</span>
           </div>
-        </footer>) : null
-    );
+
+          <div className="icon-row">
+            <i className="fa fa-cubes footer-icons" aria-hidden="true" />
+            <span className="footer-text">Racks</span>{" "}
+          </div>
+          <div className="icon-row">
+            <i className="fa fa-qrcode footer-icons" aria-hidden="true" />
+            <span className="footer-text">QrScan</span>
+          </div>
+          <div className="icon-row">
+            <i
+              className="fa fa-shopping-cart footer-icons"
+              aria-hidden="true"
+            />
+            <span className="footer-text">Cart</span>
+          </div>
+      </footer>
+    ) : null;
   }
 }
 
