efc8766d3dcb4e90e34bb11c0f2d5736d72060aa
Mayank Srivastava <MayankSri@hcl.com>
2019-08-07 16:13:07 +0530

    Redesign

--- src/components/managerDashboard/managerDashboard.js
+++ src/components/managerDashboard/managerDashboard.js
@@ -42,39 +42,17 @@ class ManagerDashboard extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="container manager-dashboard">
-          {/* <div className="row">
-            <div className="col-12 col-md-12 col-sm-12 col-xs-12 list-rack-header">
-              <span className="list-rack-header-content">
-                Associated Racks
-            </span>
-            </div>
-          </div> */}
+        <div className="manager-dashboard">
           {this.testObject.map((data, index) => {
             return (
               <div key={index}>
-                <div className="row rack-content" >
-                  <div className="col-5 col-md-5 col-sm-5 col-xs-5">
-                    <Link to="/dashbardDescription"><img
-                      src={rackImage}
-                      className="rack-image"
-                      alt="Rack"
-                    /></Link>
-                  </div>
-                  <div className="col-6 col-md-6 col-sm-6 col-xs-6 rack-location">
-                    <Link to="/dashbardDescription">
-                      <span className="rack-location-content">
-                        {data.rackLocation}
-                      </span>
-                    </Link>
-                  </div>
-                  <div className="col-1 col-md-1 col-sm-1 col-xs-1 rack-to-description">
-                    <div className="order-button-head">
-                      <i className="fa fa-angle-down" data-toggle="collapse" href={`#collapseId${index}`} role="button" aria-expanded="false" aria-controls="collapseExample" aria-hidden="true"></i>
-                    </div>
+                <div className="media rack-content">
+                  <Link to="/dashbardDescription"><img src={rackImage} className="mr-3 thumb-img" alt="..." /></Link>
+                  <div className="media-body">
+                    <Link to="/dashbardDescription">{data.rackLocation}</Link>
+                    <i className="fa fa-angle-down icon-pos" data-toggle="collapse" href={`#collapseId${index}`}></i>
                   </div>
                 </div>
-
                 <div className="collapse" id={`collapseId${index}`}>
                   <div className="card card-body">
                     <p>this is test paragraph this is test paragraphthis is test paragraphthis is
