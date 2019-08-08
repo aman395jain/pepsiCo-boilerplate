fb621f15b6727c12d746e1a530eb7babcac43bc7
Mayank Srivastava <MayankSri@hcl.com>
2019-08-08 17:14:35 +0530

    Active color in internal header

--- src/shared/internalHeader/internal-header.js
+++ src/shared/internalHeader/internal-header.js
@@ -1,16 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom"
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import classnames from "classnames";
 import "./internal-header.scss";
 
-const InternalHeader = () => {
-  return (
-    <header className="align-items-center internal-header d-flex justify-content-between">
-      <Link className = "active" to = "/managerDashboard">Home</Link>
-      <Link to = "/addRack">Add Rack</Link>
-      <Link to = "/removeRack">Remove Rack</Link>
-    </header>
-  );
+class InternalHeader extends Component {
+  state = {
+    selectedTab: "Home"
+  }
+  iHeaderJSON = [
+    {
+      "name": "Home",
+      "navigation": "/managerDashboard"
+    }, {
+      "name": "Add Rack",
+      "navigation": "/addRack"
+    }, {
+      "name": "Remove Rack",
+      "navigation": "/removeRack"
+    },
+  ];
+  setSelected(data) {
+    this.setState({ selectedTab: data.name });
+  }
+
+  refCallback(node) {
+    console.log(node)
+    node.click = () => {
+      node.focus();
+    }
+  }
+  render() {
+    return (
+      <header className="align-items-center internal-header d-flex justify-content-between">
+        {this.iHeaderJSON.map((data, index) => {
+          return (
+            <Link key={index} className={classnames({ active: this.state.selectedTab === data.name })} to={data.navigation} onClick={() => this.setSelected(data)}>{data.name}</Link>
+          );
+        })}
+      </header>
+    );
+  }
 };
 
 export default InternalHeader;
