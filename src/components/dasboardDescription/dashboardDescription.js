bd1baa39e355f06bd2dc3013cd802a422091100d
Aman Jain <AmanJa@hcl.com>
2019-08-08 17:17:11 +0530

    minor changes

--- src/components/dasboardDescription/dashboardDescription.js
+++ src/components/dasboardDescription/dashboardDescription.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Header from "../../shared/header/header";
 import Footer from "../../shared/footer/footer";
+import "./_dashboardDescription.scss";
 
 const DashboardDescription = () => {
   return (
     <div>
       <Header isAuthorized="true" />
       <div className="container manager-dashboard">
+        <p>Work Under Process...</p>
+        {/* <p>This is DashboardDescription Page............</p>
         <p>This is DashboardDescription Page............</p>
         <p>This is DashboardDescription Page............</p>
         <p>This is DashboardDescription Page............</p>
         <p>This is DashboardDescription Page............</p>
-        <p>This is DashboardDescription Page............</p>
-        <p>This is DashboardDescription Page............</p>
-        <p>This is DashboardDescription Page............</p>
+        <p>This is DashboardDescription Page............</p> */}
       </div>
       <Footer isAuthorized="true" />
     </div>
