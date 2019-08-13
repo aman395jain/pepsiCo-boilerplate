b6030f9fcf0aa6aab1d081251da1f60e6dfc2ead
Mayank Srivastava <MayankSri@hcl.com>
2019-08-13 16:54:48 +0530

    Horizontal scroll

--- src/components/dasboardDescription/dashboardDescription.js
+++ src/components/dasboardDescription/dashboardDescription.js
@@ -9,55 +9,160 @@ const DashboardDescription = () => {
   return (
     <div>
       <Header isAuthorized="true" />
-      <div className="container">
+      <div className="container dashboard-description">
         <ul id="work_illustration">
-          <li className="first">
-            <a href="tinyvoyageprints/">
-              <img
-              className = "images-logo"
-                src= {laysLogo}
-                alt="..."
-                title="A Tiny Voyage - Print Set"
-              />
-            </a>
-            <div className="new" />
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "100%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt="..."
+              title="A Tiny Voyage - Print Set"
+            />
           </li>
 
           <li>
-            <a href="tito/">
-              <img
-              className = "images-logo"
-                src= {laysLogo}
-                alt=".."
-                title="Tito Ticket Machine"
-              />
-            </a>
-            <div className="new" />
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "70%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt=".."
+              title="Tito Ticket Machine"
+            />
+          </li>
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "90%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt=".."
+              title="Tito Ticket Machine"
+            />
           </li>
 
           <li>
-            <a href="penguin-print/">
-              <img
-              className = "images-logo"
-                src={laysLogo}
-                alt="..."
-                title="Penguin Print"
-              />
-            </a>
-            <div className="new" />
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "10%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt="..."
+              title="Penguin Print"
+            />
           </li>
 
-          {/* <li className="last">
-            <a href="christmascards-2012/">
-              <img
-                src="laysLogo"
-                alt="...."
-                title="Setup at Noon Worldwide"
-              />
-              <h4>Christmas Cards 2012</h4>
-            </a>
-          </li> */}
+
         </ul>
+        <ul id="work_illustration">
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "40%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt="..."
+              title="A Tiny Voyage - Print Set"
+            />
+          </li>
+
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "80%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt=".."
+              title="Tito Ticket Machine"
+            />
+          </li>
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "30%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt=".."
+              title="Tito Ticket Machine"
+            />
+          </li>
+
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "30%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt="..."
+              title="Penguin Print"
+            />
+          </li>
+
+
+        </ul>
+
+        <ul id="work_illustration">
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "30%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt="..."
+              title="A Tiny Voyage - Print Set"
+            />
+          </li>
+
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "100%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt=".."
+              title="Tito Ticket Machine"
+            />
+          </li>
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "10%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt=".."
+              title="Tito Ticket Machine"
+            />
+
+          </li>
+
+          <li>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ "width": "100%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <img
+              className="images-logo"
+              src={laysLogo}
+              alt="..."
+              title="Penguin Print"
+            />
+          </li>
+
+
+        </ul>
+
       </div>
       <Footer isAuthorized="true" />
     </div>
