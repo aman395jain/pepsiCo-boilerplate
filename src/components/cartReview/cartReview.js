11e0acd8453bce13acbaf873c2bffc5a5992ecce
Aman Jain <AmanJa@hcl.com>
2019-08-20 16:25:23 +0530

    product API integration

--- src/components/cartReview/cartReview.js
+++ src/components/cartReview/cartReview.js
@@ -25,30 +25,45 @@ class CartReview extends Component {
   render() {
     return (
       <div className="cart-review">
-        {this.state._rackData.map((data, index) => {
-          return data.approvedStatus !== "approved" ? (
-            <Swipeout
-              right={[
-                {
-                  text: "Approve",
-                  onPress: () => {
-                    this.approveOrder();
+        {this.state._rackData &&
+          this.state._rackData.map((data, index) => {
+            return data.approvedStatus !== "approved" ? (
+              <Swipeout
+                right={[
+                  {
+                    text: "Approve",
+                    onPress: () => {
+                      this.approveOrder();
+                    },
+                    style: { backgroundColor: "#28a745", color: "white" },
+                    className: "custom-class-1"
                   },
-                  style: { backgroundColor: "#54b226", color: "white" },
-                  className: "custom-class-1"
-                },
-                {
-                  text: "Reject",
-                  onPress: () => {
-                    this.rejectOrder();
-                  },
-                  style: { backgroundColor: "#ff690d", color: "white" },
-                  className: "custom-class-2"
-                }
-              ]}
-              key={index}
-            >
-              <div style={{ height: 120 }}>
+                  {
+                    text: "Reject",
+                    onPress: () => {
+                      this.rejectOrder();
+                    },
+                    style: { backgroundColor: "#ff690d", color: "white" },
+                    className: "custom-class-2"
+                  }
+                ]}
+                key={index}
+              >
+                <div style={{ height: 120 }}>
+                  <div className="media rack-content">
+                    <img src={rackImage} className="mr-3 thumb-img" alt="..." />
+
+                    <div className="media-body">
+                      <div className="content-center">
+                        <strong>{data.name}</strong>
+                        <p>{data.locationName}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Swipeout>
+            ) : (
+              <div key={index} style={{ height: 120, marginTop: 10 }}>
                 <div className="media rack-content">
                   <img src={rackImage} className="mr-3 thumb-img" alt="..." />
 
@@ -57,27 +72,12 @@ class CartReview extends Component {
                       <strong>{data.name}</strong>
                       <p>{data.locationName}</p>
                     </div>
+                    <button className="btn btn-success btn-approved" />
                   </div>
                 </div>
               </div>
-            </Swipeout>
-          ) : (
-            <div key={index} style={{ height: 120, marginTop: 10 }}>
-              <div className="media rack-content">
-                <img src={rackImage} className="mr-3 thumb-img" alt="..." />
-
-                <div className="media-body">
-                  <div className="content-center">
-                    <strong>{data.name}</strong>
-                    <p>{data.locationName}</p>
-                  </div>
-                  <button className = "btn btn-success btn-approved"></button>
-                </div>
-
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     );
   }
--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,93 +1,85 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom"
-import classnames from 'classnames';
+import classnames from "classnames";
 
 import "./_products.scss";
 // import rackImage from "../../assets/images/Rack_Image.png";
 import rackImage from "../../assets/images/vending.jpg";
-import Header from "../../shared/header/header"
+import Header from "../../shared/header/header";
 import Footer from "../../shared/footer/footer";
-import { rackDataService } from "../../services/rackData.service";
+import { productDataService } from "../../services/productsData.service";
 import laysLogo from "../../assets/images/image/barbecue.png";
 import doritos from "../../assets/images/image/doritos.png";
-import Rating from "react-rating"
+import Rating from "react-rating";
 
 class Products extends Component {
+  state = {
+    openDiv: false,
+    isLogin: true,
+    arrowClass: "fa fa-angle-down",
+    isActive: false,
+    _rackData: []
+  };
 
+  componentDidMount() {
+    productDataService.getAllProducts().then(result => {
+      console.log("in the product API data", result.data);
+      this.setState({
+        _rackData: result.data
+      });
+    });
+  }
 
-    state = {
-        openDiv: false,
-        isLogin: true,
-        arrowClass: "fa fa-angle-down",
-        isActive: false,
-        _rackData: [
-            { id: 1, name: "Pepsico Parkwood Rack 1", locationName: "North Kitchen", longDescription: "All Top Products" },
-            { id: 2, name: "Second Rack", locationName: "Kitchen South", longDescription: "All Top Products2" },
-            { id: 3, name: "Second Rack3", locationName: "Kitchen South3", longDescription: "All Top Products3" },
-            { id: 4, name: "Second Rack4", locationName: "Kitchen South4", longDescription: "All Top Products4" },
-            { id: 5, name: "Second Rack5", locationName: "Kitchen South5", longDescription: "All Top Products5" }
-        ]
+  displayDivData = () => {
+    if (!this.state.openDiv) {
+      this.setState({ openDiv: true });
+    } else {
+      this.setState({ openDiv: false });
     }
+  };
 
-    /* constructor(props) {
-      super(props);
-      
-    } */
-    componentDidMount() {
-        // rackDataService.getAllRacks().then(result => {
-        //     this.setState({
-        //         _rackData: result.data
-        //     })
-        // })
-
-    }
-
-    displayDivData = () => {
-
-        if (!this.state.openDiv) {
-            this.setState({ openDiv: true })
-        } else {
-            this.setState({ openDiv: false })
-        }
-    }
-
-    render() {
-        console.log("_rackData :::::: ", this.state._rackData)
-        return (
-            <React.Fragment>
-                <div>
-                    <Header isAuthorized={this.state.isLogin} />
-                    <div className="manager-dashboard">
-                        {this.state._rackData && this.state._rackData.map((data, index) => {
-                            return (
-                                <div key={index} className={classnames("media-wrapper", { 'active': this.state.isActive })}>
-                                    <div className="media rack-content" style={{ paddingRight: "0" }}>
-                                        <Link to="/dashbardDescription"><img height="90" src={laysLogo} className="mr-3 thumb-img" alt="..." /></Link>
-                                        <div className="media-body">
-                                            <div className="content-center">
-                                                <Link to="/dashbardDescription">
-                                                    <strong>{data.name}</strong>
-                                                    <p>{data.locationName}</p>
-                                                    <p className={classnames({ 'green': data.currentMode === "INVENTORY" })}>{data.currentMode}</p>
-
-                                                </Link>
-                                                <Rating emptySymbol="far fa-star" fullSymbol="fa fa-star" />
-                                                <span class="btn btn-danger circle icon-pos" style={{ right: "20px", zIndex: -2000 }}></span>
-                                                {/* <i className="fa fa-angle-down icon-pos" data-toggle="collapse" id={`#collapseId${index}`} href={`#collapseId${index}`} onClick={() => this.changeIcon(index)}></i> */}
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            );
-                        })}
-
+  render() {
+    return (
+      <React.Fragment>
+        <div className="manager-dashboard">
+          {this.state._rackData &&
+            this.state._rackData.map((data, index) => {
+              return (
+                <div
+                  key={index}
+                  className={classnames("media-wrapper", {
+                    active: this.state.isActive
+                  })}
+                >
+                  <div className="media" style={{ paddingRight: "0" }}>
+                    <img
+                      height="90"
+                      src={data.impageURL}
+                      className="mr-3 thumb-img"
+                      alt="..."
+                    />
+                    <div className="media-body">
+                      <div className="content-center">
+                        <strong>{data.shortDescription}</strong>
+                        <p style={{ width: "80%" }}>{data.longDescription}</p>
+                        <Rating
+                          emptySymbol="far fa-star"
+                          fullSymbol="fa fa-star"
+                        />
+                        <span
+                          className="btn btn-danger circle icon-pos"
+                          style={{ right: "20px", zIndex: -2000 }}
+                        />
+                        {/* <i className="fa fa-angle-down icon-pos" data-toggle="collapse" id={`#collapseId${index}`} href={`#collapseId${index}`} onClick={() => this.changeIcon(index)}></i> */}
+                      </div>
                     </div>
-                    <Footer isAuthorized={this.state.isLogin} />
+                  </div>
                 </div>
-            </React.Fragment >
-        );
-    }
+              );
+            })}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Products;
