78f9a367e2f0cd06ba8215f3c0c6643e05a37096
Aman Jain <AmanJa@hcl.com>
2019-08-22 17:09:20 +0530

    minor changes

--- src/services/productsData.service.js
+++ src/services/productsData.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 let userLoginToken = "Bearer" + " " + localStorage.getItem("user-login-jwt");
 const headerConfig = {
   headers: {
@@ -7,13 +8,15 @@ const headerConfig = {
   }
 };
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const productApi = "http://gateway.och.40.122.144.129.nip.io/services/product/api/";
+const productApi =
+  "http://gateway.och.40.122.144.129.nip.io/services/product/api/";
+
 export const productDataService = {
   getAllProducts: async () => {
     let callAPI = await axios.get(
       proxyurl + productApi + "/products",
       headerConfig
-    )
+    );
     return callAPI;
   }
 };
